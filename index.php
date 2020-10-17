f2ca94d5fed0c1a5a53047c0f7864c3e6df8cc4e
julien leger <julienleger2000@yahoo.fr>
2020-10-17 12:08:54 +0200

    cy

--- index.php
+++ index.php
@@ -1,34 +1,6 @@
 <?php
-//inclu getBdd()
+
 require_once './config.php';
 require_once './City.php';
-?>
-<h1>World</h1>
-<ul>
-    <li><a href="country.php">Country</a></li>
-    <li><a href="crudCity/read.php">City</a></li>
-    <li><a href="languages.php">Languages</a></li>
-</ul>
-<h2>Add user</h2>
-<?php
-if (isset($_POST['firstname'])) {
-    $user = City::find($_POST['id']);
-    $user->populate($_POST);
-    //$user = new User($_POST);
-    $user->save();
-    echo 'utilisateur ' . $user->firstname . ' ' . $user->lastname . ' enregistré sous l\'id ' . $user->id;
-}
-
-$cities = City::findAll();
-?>
-<form method="post">
-    <select name="id">
-        <?php foreach ($cities as $city) { ?>
-            <option value="<?php echo $city->id ?>"><?php echo $city->name ?></option>
-        <?php } ?>
-    </select>
-    <label>Firstname :</label><input type="text" name="firstname"><br>
-    <label>Lastname :</label><input type="text" name="lastname"><br>
-    <input type="submit">
-</form>
-<?php
+$view = 'Accueil';
+require_once("./tpl/index.phtml");
